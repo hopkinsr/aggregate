aa51ed01e498f1e0605dbca392ef1f9675259af3
Richard Hopkins <hopkinsr@server.fake>
2014-01-26 17:49:21 +0000

    Lay foundations for a Racket package compatible with 5.3.5+ and 6.0+
    NOTE: We're still far off being stable and ready for release.

--- README.md
+++ README.md
@@ -1,110 +1,11 @@
 aggregate
 =========
 
-IMPORTANT: Currently in development. There may be breakages before the
-first release of this as a proper Racket 6 package.
+IMPORTANT: Currently in development. There will almost definitely be breakages
+and out-of-date documentation before the first release of this as a proper
+Racket package.
 
-A Racket library to implement and use SQL-style aggregate functions over a stream of values, as well as simple wrappers for the math/statistics modules for use in exploratory analysis.
+A Racket library which implements and provides a Racket generics interface
+for SQL-style aggregate operations, as well as common operations to group
+values Mathematica-style.
 
-Appetiser
----------
-
-Lets get tasting (see manual.scrbl for more information). First, some standard definitions.
-
-Examples:
-
-    > (struct product (section name quantity))
-    > (define shop (list (product 'food 'chocolate-bar 5)
-                         (product 'literature 'magazine 4)
-                         (product 'food 'bonbons 20)))
-
-Some common statistics for a range of numbers.
-
-Examples:
-
-    > (define xs (range 10))
-    > (aggregate xs)
-
-    (list (aggregate/count 10 #<procedure:const>))
-
-    > (aggregate xs (list (-->min)
-                          (-->max)
-                          (-->count)
-                          (-->sum)))
-
-    (list (aggregate/min 0 #<procedure:identity> #<procedure:<> 0)
-          (aggregate/max 9 #<procedure:identity> #<procedure:>> 9)
-          (aggregate/count 10 #<procedure:const>)
-          (aggregate/sum 45 #<procedure:identity>))
-
-    > (aggregate/summary xs)
-
-    '((count 10) (min 0) (max 9) (mean 9/2))
-
-Total count of products, and the minimum quantity.
-
-Example:
-
-    > (aggregate/agg-val shop
-                         (list (-->count)
-                               (-->min #:key product-quantity)))
-
-    '(3 4)
-
-First and last countries if we listed them lexicographically.
-
-Example:
-
-    > (aggregate/agg-val (list "Germany" "Argentina" "Japan" "Zimbabwe")
-                         (list (-->min #:<operator string<?)
-                               (-->max #:>operator string>?)))
-
-    '("Argentina" "Zimbabwe")
-
-Simple binning of numbers.
-
-Example:
-
-    > (group '(5 5 5 5 6 7 7 8 8 8))
-
-    (hash 5 (list (aggregate/count 4 #<procedure:const>))
-          6 (list (aggregate/count 1 #<procedure:const>))
-          7 (list (aggregate/count 2 #<procedure:const>))
-          8 (list (aggregate/count 3 #<procedure:const>)))
-
-Slightly more advanced binning of numbers: we want to know how many are odd and even in a given range, along with the respective sums.
-
-Example:
-
-    > (group (range 10)
-             #:key even?
-             #:aggregates (thunk (list (-->count)
-                                       (-->sum))))
-
-    (hash #t (list (aggregate/count 5 #<procedure:const>)
-                   (aggregate/sum 20 #<procedure:identity>))
-          #f (list (aggregate/count 5 #<procedure:const>)
-                   (aggregate/sum 25 #<procedure:identity>)))
-
-Example:
-
-    > (group/agg-val (range 10)
-                     #:key even?
-                     #:aggregates (thunk (list (-->count)
-                                               (-->sum))))
-
-    '#hash((#t . (5 20)) (#f . (5 25)))
-
-They don’t have to be numbers. We want to know how many different products do we have in each section, along with how much stock.
-
-Example:
-
-    > (group shop
-             #:key product-section
-             #:aggregates (thunk (list (-->count)
-                                       (-->sum #:key product-quantity))))
-
-    (hash 'food (list (aggregate/count 2 #<procedure:const>)
-                      (aggregate/sum 25 #<procedure:product-quantity>))
-          'literature (list (aggregate/count 1 #<procedure:const>)
-                            (aggregate/sum 4 #<procedure:product-quantity>)))
--- aggregate/main.rkt
+++ aggregate/main.rkt
@@ -1,9 +1,9 @@
 #lang racket
 ;;; aggregate
 ;;;
-;;; A Racket library to implement and use SQL-style aggregate functions over
-;;; a stream of values, as well as simple wrappers for the math/statistics
-;;; modules for use in exploratory analysis.
+;;; A Racket library which implements and provides a Racket generics interface
+;;; for SQL-style aggregate operations, as well as common operations to group
+;;; values Mathematica-style.
 ;;;
 ;;; Order of the file
 ;;;
@@ -12,8 +12,6 @@
 ;;; * standard aggregates
 ;;; * constructor wrappers
 ;;; * aggregate building operations + wrappers
-;;; * math/statistics wrappers
-;;; * rackunit tests
 ;;; 
 
 (require racket/generic)
@@ -62,14 +60,6 @@
                           (hash/c any/c list?))]
           [gather-by/values (->* (sequence?) (#:key (-> any/c any))
                                  (listof list?))]))
-
-; math/statistics wrappers
-(provide (contract-out
-          [statistics-summary (-> statistics? (listof (list/c symbol? flonum?)))]
-          [statistics-extended-summary (-> statistics? (listof (list/c symbol? flonum?)))]
-          [collect-statistics (-> sequence? statistics?)]
-          [collect-statistics/summary (-> sequence? (listof (list/c symbol? flonum?)))]
-          [collect-statistics/extended-summary (-> sequence? (listof (list/c symbol? flonum?)))]))
 
 ;;;
 ;;; types and interfaces
@@ -316,113 +306,3 @@
   (define gathered (gather-by xs #:key key))
   (hash-values gathered))
 
-;;;
-;;; math/statistics wrappers
-;;;
-
-;; Returns a summary containing the count, min, max and mean.
-; (statistics-summary statistics) -> (list (list symbol? flonum?) ...)
-(define (statistics-summary s)
-  (list (list 'count (statistics-count s))
-        (list 'min (statistics-min s))
-        (list 'max (statistics-max s))
-        (list 'mean (statistics-mean s))))
-
-;; Returns an extended summary containing the standard summary
-;; (statistics-summary) as well as range, stddev, variance, skewness
-;; and kurtosis.
-; (statistics-summary statistics) -> (list (list symbol? flonum?) ...)
-(define (statistics-extended-summary s)
-  (append (statistics-summary s)
-          (list (list 'range (statistics-range s))
-                (list 'stddev (statistics-stddev s))
-                (list 'variance (statistics-variance s))
-                (list 'skewness (statistics-skewness s))
-                (list 'kurtosis (statistics-kurtosis s)))))
-
-;; Calculates statistics after visiting all of the values in xs
-; (collect-statistics sequence) -> statistics
-(define (collect-statistics xs)
-  (update-statistics* empty-statistics xs))
-
-;; Calculates statistics after visiting all of the values in xs and returns
-;; a basic summary. See statistics-summary.
-; (collect-statistics/summary sequence) -> (list (list symbol? flonum?) ...)
-(define (collect-statistics/summary xs)
-  (define s (collect-statistics xs))
-  (statistics-summary s))
-
-;; Calculates statistics after visiting all of the values in xs and returns
-;; an extended summary. See statistics-extended-summary
-; (collect-statistics/extended-summary sequence) -> (list (list symbol? flonum?) ...)
-(define (collect-statistics/extended-summary xs)
-  (define s (collect-statistics xs))
-  (statistics-extended-summary s))
-
-;;;
-;;; rackunit tests
-;;;
-(module+ test
-  (require rackunit)
-  (require math/statistics)
-  
-  ; helpers for testing based on standard functions provided by racket
-  (define (check-aggregate xs agg standard-function)
-    (check-equal? (aggregate/agg-val xs (list (agg)))
-                  (list (standard-function xs))))
-  
-  (define (check-aggregate/apply xs agg standard-function)
-    (check-equal? (aggregate/agg-val xs (list (agg)))
-                  (list (apply standard-function xs))))
-  
-  (define xs (range 100))
-  
-  (check-aggregate xs -->count length)
-  (check-aggregate/apply xs -->sum +)
-  (check-aggregate/apply xs -->min min)
-  (check-aggregate/apply xs -->max max)
-  (check-aggregate xs -->mean mean)
-  
-  (check-equal? (aggregate/agg-val (range 10)
-                                   (list (-->min) (-->max) (-->count) (-->sum) (-->mean)))
-                (list 0 9 10 45 9/2))
-  
-  (check-equal? (group/agg-val (range 10)
-                               #:key (const #t)
-                               #:aggregates (thunk (list (-->min) (-->max) (-->count) (-->sum) (-->mean))))
-                (make-hash (list '(#t . (0 9 10 45 9/2)))))
-  
-  (check-equal? (group/agg-val (range 10)
-                               #:key even?
-                               #:aggregates (thunk (list (-->count)
-                                                         (-->sum))))
-                (make-hash (list '(#t . (5 20)) '(#f . (5 25)))))
-  
-  (check-equal? (group/agg-val (range 10)
-                               #:key even?
-                               #:aggregates (thunk (list (-->count)
-                                                         (-->list))))
-                (make-hash (list '(#t . (5 (0 2 4 6 8)))
-                                 '(#f . (5 (1 3 5 7 9))))))
-  
-  (check-equal? (gather-by (range 10)
-                           #:key even?)
-                (make-hash (list '(#t . (0 2 4 6 8))
-                                 '(#f . (1 3 5 7 9)))))
-  
-  (check-equal? (gather-by/values (range 10)
-                                  #:key even?)
-                (list '(0 2 4 6 8)
-                      '(1 3 5 7 9)))
-  
-  (check-equal? (gather-by (range 10)
-                           #:key (λ (x) (modulo x 3)))
-                (make-hash (list '(0 . (0 3 6 9))
-                                 '(1 . (1 4 7))
-                                 '(2 . (2 5 8)))))
-  
-  (check-equal? (tally (range 10)
-                       #:key even?)
-                (make-hash (list '(#t . 5) '(#f . 5))))
-  (check-equal? (tally '(5 5 5 5 6 7 7 8 8 8))
-                (make-hash (list '(5 . 4) '(6 . 1) '(7 . 2) '(8 . 3)))))
